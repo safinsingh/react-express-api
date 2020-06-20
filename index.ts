a7c6a6ea48b73dd85412aeb97fa499e01d809487
safinsingh <safin.singh@gmail.com>
2020-06-20 13:07:39 -0700

    add types to index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
+import { Response, Request, Express } from "express";
 const express = require("express");
 const path = require("path");
 
-const app = express();
+const app: Express = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
 // Put all API endpoints under '/api'
-app.get("/api/", (req, res) => {
-  const data = {
+app.get("/api/", (req: Request, res: Response) => {
+  const data: Object = {
     users: ["bruh", "bruh2"],
   };
 
@@ -18,7 +19,7 @@ app.get("/api/", (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get("*", (req, res) => {
+app.get("*", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
